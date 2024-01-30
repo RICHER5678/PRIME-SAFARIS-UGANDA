021b23ed11cc83212a90d2f3756300b6d9ba863e
BAGOMBEKA JOB <81092163+RICHER5678@users.noreply.github.com>
2024-01-30 15:04:37 +0300

    vscode
    FIRST COMMIT

---
+++ header.php
@@ -0,0 +1,55 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>PRIME SAFARIS UGANDA</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"> 
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <div class="container-fluid position-relative nav-bar p-0">
+        <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
+            <nav class="navbar navbar-expand-lg bg-light navbar-light shadow-lg py-3 py-lg-0 pl-3 pl-lg-5">
+                <a href="" class="navbar-brand">
+                    <h1 class="m-0 text-success"><span class="text-success">PRIME SAFARIS </span></h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse text-success justify-content-between px-3" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto py-0">
+                        <a href="index.php" class="nav-item nav-link">Home</a>
+                        <a href="index.php" class="nav-item nav-link active">Kenya Safaris</a>
+                        <a href="index.php" class="nav-item nav-link">Tanzania Safaris</a>
+                        <a href="index.php" class="nav-item nav-link">Uganda Safaris</a>
+                      
+                        
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+include("header.php");
+?>
+
+
+
+    
+
+
+ 
+
+
+    
+
+
+    <!-- Service Start -->
+    <div style="background-color:#ffa33a;" class="container-fluid py-5">
+        <div class="container pt-5 pb-3">
+            <div class="text-center mb-3 pb-3">
+                
+                
+            </div><br>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="service-item bg-white text-center mb-2 py-5 px-4">
+                       
+                       
+                        <p class="m-0">
+                        Prime Safaris Uganda, a legally registered tours and travel company with its main offices in Uganda. We offer Wildlife Safaris and Excursions in Uganda, Kenya, Tanzania and Rwanda. With a focus on sustainable and responsible travel. We offer unique experiences and customized itineraries to ensure an 
+                        unforgettable journey through East Africa’s stunning landscapes and diverse wildlife.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="service-item bg-white text-center mb-2 py-5 px-4">
+                       
+                        <p class="m-0">
+                        Prime Safaris Uganda, a legally registered tours and travel company with its main offices in Uganda. We offer Wildlife Safaris and Excursions in Uganda, Kenya, Tanzania and Rwanda. With a focus on sustainable and responsible travel. We offer unique experiences and customized itineraries to ensure an unforgettable journey through East Africa’s stunning landscapes and diverse wildlife.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="service-item bg-white text-center mb-2 py-5 px-4">
+                        
+                        <p class="m-0"> 
+                        Prime Safaris Uganda, a legally registered tours and travel company with its main offices in Uganda. We offer Wildlife Safaris and Excursions in Uganda, Kenya, Tanzania and Rwanda. With a focus on sustainable and responsible travel. We offer unique experiences and customized itineraries to ensure an unforgettable journey through East Africa’s stunning landscapes and diverse wildlife.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Service End -->
+
